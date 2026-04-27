a93b67409377e53738c03d71d37e94be320831ec
tad0025 <datt32285@gmail.com>
2026-04-27 09:25:02 +0700

    feat: add import

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import { useAuth } from '../context/AuthContext';
 
